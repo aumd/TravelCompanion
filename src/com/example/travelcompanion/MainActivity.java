3cbb2c2275b1dab14099fa64cb867dd0d39fcc04
Alyzza <lyrexzalyzza@gmail.com>
2014-02-12 13:06:07 +0800

    Load Map

--- src/com/example/travelcompanion/MainActivity.java
+++ src/com/example/travelcompanion/MainActivity.java
@@ -2,21 +2,20 @@ package com.example.travelcompanion;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
+ 
+ WebView myBrowser;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_main);
+  
+  myBrowser = (WebView)findViewById(R.id.mybrowser);
+  myBrowser.loadUrl("file:///android_asset/simplemap.html");
+  myBrowser.getSettings().setJavaScriptEnabled(true);
+ }
 
 }
