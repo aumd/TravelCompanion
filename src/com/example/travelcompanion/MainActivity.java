77f63b1cbf85d5b77909b784181dc46f0810de57
Elisa May Comeda <metotcom@gmail.com>
2014-02-23 20:45:07 -0800

    Edited Main Activity

--- src/com/example/travelcompanion/MainActivity.java
+++ src/com/example/travelcompanion/MainActivity.java
@@ -2,11 +2,31 @@ package com.example.travelcompanion;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.WindowManager;
 import android.webkit.WebView;
+
 
 public class MainActivity extends Activity {
  
  WebView myBrowser;
+
+ // Comment sa ni for now. Para ni sa home button unta.
+ //@Override
+// public void onAttachedToWindow() {
+//	 if(MyService.Flag == true){
+
+         // TODO Auto-generated method stub
+ //        this.getWindow().setType(
+ //                WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG
+ //                        | WindowManager.LayoutParams.FLAG_FULLSCREEN);
+ //        this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD);
+
+ //    super.onAttachedToWindow();
+//	    }else{
+//	        finish();
+//	    }
+//	}
+
 
  @Override
  protected void onCreate(Bundle savedInstanceState) {
